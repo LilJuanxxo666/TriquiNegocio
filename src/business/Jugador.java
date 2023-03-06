f3c716b7b64c3dbe9e3e2a094a0fdd823c104d89
Juan Fernando <wuanter.castano@gmail.com>
2023-03-06 13:21:59 -0500

    Actualizaciones pequenas en la clase Jugador

--- src/business/Jugador.java
+++ src/business/Jugador.java
@@ -2,34 +2,45 @@ package business;
 
 public class Jugador {
 
-    private String jugador, simbolo;
+    private String nombreJugador, simbolo;
     public Jugador(String simbolo) {
         this.simbolo = setSimbolo(simbolo);
     }
-    public Jugador(String jugador, String simbolo) {
+    public Jugador(String nombreJugador, String simbolo) {
         this.simbolo = setSimbolo(simbolo);
-        this.jugador = setJugador(jugador);
+        this.nombreJugador = setJugador(nombreJugador);
     }
     private String setSimbolo(String simbolo){
         if(simbolo.trim().equals("")) {
             throw new RuntimeException("No es posible poner un simbolo vacio o nulo");
         }
-        if(simbolo.trim().length() != 1) {
+        else if(simbolo.trim().length() != 1) {
             throw new RuntimeException("No es posible poner un simbolo con longitud > 1");
         }
         else{
             return simbolo;
         }
     }
-    private String setJugador(String jugador){
-        if(jugador.trim().equals("")) {
+    private String setJugador(String nombreJugador){
+        if(nombreJugador.trim().equals("")) {
             throw new RuntimeException("No es posible poner un nombre de jugador vacio o nulo");
         }
-        if(jugador.trim().length() > 8) {
+        else if(nombreJugador.trim().length() > 8) {
             throw new RuntimeException("No es posible poner un nombre con una longitud > 1");
         }
         else{
-            return jugador;
+            return nombreJugador;
         }
     }
+    public String getSimbolo(){
+        return simbolo;
+    }
+
+    public String getNombreJugador(){
+        return nombreJugador;
+    }
+    public Jugador getJugador(){
+        return new Jugador(getNombreJugador(), getSimbolo());
+    }
+
 }
